9822717ab4c01b31421952e80192ab6fab9a1b1c
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-04-29 15:12:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
- [element, ...array]
+array = [element, ...array]
  return array
 }
 
