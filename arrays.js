586e977b99c127034b4535f8925805f8e4897a1a
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-04-29 15:03:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  ["hershey's", ...chocolateBars]
+  [element, ...chocolateBars]
   return chocolateBars
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  chocolateBars.unshift("hershey's")
+  chocolateBars.unshift()
   return chocolateBars
 }
