d918f2c64807b843918bd73a9e3fdf7055db0be2
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-04-29 15:35:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(-2)
+  array = array.slice(1)
   return array
 }
