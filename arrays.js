b47f6b22c60ac0fcde75e6a0c4213e01bcf30be9
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-04-29 15:29:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift(element)
+  return array
+}
